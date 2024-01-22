777d7921df066e293204711f62460b62161b197e
lets go <84551063+mesophil@users.noreply.github.com>
2024-01-21 21:02:28 -0700

    picam test

--- vision/detect_pi.py
+++ vision/detect_pi.py
@@ -4,9 +4,6 @@ import math
 import logging
 import numpy as np
 
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-
 from config import confThresh, xDim, yDim, classNames, modelPath
 
 
@@ -15,68 +12,37 @@ logging.basicConfig(filename='my.log', format='%(asctime)s : %(levelname)s : %(m
 def main():
     logging.info('Loading cameras')
 
-    cam0 = cv2.VideoCapture(0)
+    cam0 = cv2.VideoCapture('/dev/video0')
     cam0.set(3, xDim)
     cam0.set(4, yDim)
 
-    cam1 = cv2.VideoCapture(1)
-    cam1.set(3, xDim)
-    cam1.set(4, yDim)
+    # cam1 = cv2.VideoCapture('/dev/video1')
+    # cam1.set(3, xDim)
+    # cam1.set(4, yDim)
 
-    logging.info('Loading model')
+    # model = YOLO(modelPath)
 
-    model = YOLO(modelPath)
-
-    logging.info('Begin reading')
-
-    mode, needImage = readInputs()
-
-    while needImage and mode != 'return':
+    i = 0
+    
+    while i < 3:
         _, img0 = cam0.read()
-        _, img1 =cam1.read()
+        
+        cv2.imwrite(f'~/Desktop/c{i}.png',img0)
 
-        results = model(img0, stream=True)
+        # results = model(img0, stream=True)
 
-        if mode == 'close':
-            depthMapping = depthMap(img0, img1)
+        # for r in results:
+        #     boxes = r.boxes
 
-        for r in results:
-            boxes = r.boxes
+        #     for box in boxes:
+        #         x1, y1, x2, y2 = box.xyxy[0]
+        #         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+        #         confidence = math.ceil((box.conf[0]*100))/100
+        #         cls = int(box.cls[0])
 
-                confidence = math.ceil((box.conf[0]*100))/100
-                cls = int(box.cls[0])
-
-                if classNames[cls] == 'keyboard' and confidence >= confThresh:
-                    mid = (np.mean([x1, x2]), np.mean([y1, y2]))
-
-                    theta = calculateAngle(mid[0], mid[1])
-                    
-                    depth = -1
-                    if mode == 'close':
-                        depth = depthMapping[mid[0], mid[1]]
-                        
-                    logging.info(f"Theta: {theta}, Depth: {depth}")
-
-                    # somehow output theta (and depth if mode is close) to the controller and update parameters mode and needImage
-                    sendOutputs(theta, depth)
-
-                    # early stoppage - remove if not accurate and replace with highest confidence box
-                    break
-
-
-        mode, needImage = readInputs()
-        mode = 'return'
-        needImage = False
-
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    cam0.release()
-    cam1.release()
+        i += 1
+    
     cv2.destroyAllWindows()
 
 
