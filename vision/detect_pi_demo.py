14ca9c5c8bb1c4ff8e9a0df18964fc36b93462ee
lets go <84551063+mesophil@users.noreply.github.com>
2024-02-06 17:34:39 -0700

    Rectangle in demo

--- vision/detect_pi_demo.py
+++ vision/detect_pi_demo.py
@@ -49,6 +49,7 @@ def main():
             for box in boxes:
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+                cv2.rectangle(img0, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
                 confidence = math.ceil((box.conf[0]*100))/100
                 cls = int(box.cls[0])
