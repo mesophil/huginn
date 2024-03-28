45796d4cf388697a785ac1da9c1fde742033a76f
Hariharan-Krishnan <hkrishna@ualberta.ca>
2024-03-28 19:05:25 +0000

    bruh project done gg

--- tcp/server.py
+++ tcp/server.py
@@ -9,22 +9,29 @@ socket.bind("tcp://*:50165")
 
 import re
 
-def printData():
+def printData(message):
     sleep(1)
-    print("Hello")
+    print(message)
     return
 
 
 while True:
-     #  Wait for next request from client
-     message = socket.recv()
-     message = str(message)
+    #  Wait for next request from client
+    message = socket.recv()
+    message = str(message)
 
-     if "Hello" in message:
-        printData()
+    if "Angle" in message:
+        test = 1.05
+        socket.send(str(test).encode())
+        printData(str(test))
+    elif "XY" in message:
+        socket.send(b"20,30")
+        printData(str(message))
+    else:
+        #  Send reply back to client
+        socket.send(b"error")
 
-     sleep(5)
-     print(datetime.now(),message)
+    sleep(5)
+    print(datetime.now(),message)
 
-     #  Send reply back to client
-     socket.send(b"Data Recieved")
+     
